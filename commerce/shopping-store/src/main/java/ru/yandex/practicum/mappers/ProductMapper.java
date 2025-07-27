667c76526d72f811a78f9cf11c86bac1b5d5d594
PrettyDG <danilapr11@yandex.ru>
2025-07-28 02:23:53 +0300

    -fix

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/mappers/ProductMapper.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/mappers/ProductMapper.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Component;
 import ru.yandex.practicum.dto.shoppingStore.ProductDto;
 import ru.yandex.practicum.models.Product;
 
-@Component
 @UtilityClass
 public class ProductMapper {
     public static ProductDto toDto(Product product) {
